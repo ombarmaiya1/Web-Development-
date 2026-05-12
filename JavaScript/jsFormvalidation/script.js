8f8117ead599cff8cefb68a055872d195fe26966
ombarmaiya1 <ombarmaiya@gmail.com>
2026-05-12 20:09:42 +0530

    form-validation

---
+++ JavaScript/jsFormvalidation/script.js
@@ -0,0 +1,35 @@
+let fn = document.getElementById('fn');
+let pn = document.getElementById('pn');
+let em = document.getElementById('em');
+let ps = document.getElementById('ps');
+let sm = document.getElementById('sm');
+
+sm.addEventListener('submit',submit);
+
+function submit(e) 
+{
+    e.preventdefault();
+    let obj = {
+        'name' : fn.value,
+        'phone number' : pn.value,
+        'name' : fn.value,
+        'name' : fn.value
+        
+    }
+}
+
+let na = 'SD'
+
+
+if (!/^[A-Za-z\.\d\_]/+$) {
+    
+}
+
+function check(name)
+ {
+   const alpha =['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+   name.toLowerCase();
+
+
+}
+
